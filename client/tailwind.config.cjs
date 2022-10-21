a09963c0d59f8c47b590c9e552b993f8efdd4153
CrisD3v <crisdevenginer@gmail.com>
2022-10-21 15:47:57 -0500

    Update

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        redGray:'#FEE2E2',
+        semiRed: '#FCA5A5',
+        orange: '#C2410C',
+        redClare: '#F87171',
+        gray: '#9B9B9B',
+        white: '#FFF'
+      },
+      fontFamily: {
+      }
+    },
+  },
+  plugins: [],
+  variants:{
+    appearance: ['responsive'],
+  }
+}
