89787ddb1a0be2ff11de1b5857dad4f116278710
MThanCode <ricardobvil116@gmail.com>
2022-10-24 14:41:40 -0600

    feat(api/ config): add app db config

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,11 @@
 import server from './src/app'
+import  db from './src/db'
 // const { PORT } = process.env
 const PORT = 3001
 
-server.listen(PORT, () => {
-  console.log('%s listening at ' + PORT) // eslint-disable-line no-console
+// Syncing all the models at once.
+db.conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log('%s listening at ' + PORT) // eslint-disable-line no-console
+  })
 })
