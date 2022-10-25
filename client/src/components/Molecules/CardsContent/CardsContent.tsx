04b686bea09890a6de955e6f6ed7c3ebbc11b733
NachoBerridy <nachoberridy1997@gmail.com>
2022-10-25 11:43:22 -0300

    Mapeado de cartas en cada categoría

--- client/src/components/Molecules/CardsContent/CardsContent.tsx
+++ client/src/components/Molecules/CardsContent/CardsContent.tsx
@@ -27,6 +27,69 @@ const CardsContent = () => {
         },
     ]
 
+    const cardsData = [
+        {
+            title: 'Maquina 1',
+        },
+        {
+            title: 'Maquina 2',
+        },
+        {
+            title: 'Maquina 3',
+        },
+        {
+            title: 'Maquina 4',
+        },
+        {
+            title: 'Maquina 5',
+        },
+        {
+            title: 'Maquina 6',
+        },
+        {
+            title: 'Maquina 7',
+        },
+        {
+            title: 'Maquina 8',
+        },
+        {
+            title: 'Maquina 9',
+        },
+        {
+            title: 'Maquina 10',
+        },
+        {
+            title: 'Maquina 11',
+        },
+        {
+            title: 'Maquina 12',
+        },
+        {
+            title: 'Maquina 13',
+        },
+        {
+            title: 'Maquina 14',
+        },
+        {
+            title: 'Maquina 15',
+        },
+        {
+            title: 'Maquina 16',
+        },
+        {
+            title: 'Maquina 17',
+        },
+        {
+            title: 'Maquina 18',
+        },
+        {
+            title: 'Maquina 19',
+        },
+        {
+            title: 'Maquina 20',
+        },
+    ]
+
 
     return (
         <Routes>
@@ -39,8 +102,10 @@ const CardsContent = () => {
                     }
                 </div> />
             <Route path="*/:category" element=
-                <div className={style.container}>
-                    {<Card />} 
+                <div className="grid grid-cols-4 gap-2 w-">
+                    {cardsData.map((item) => {
+                        return <Card data={item} />
+                    })}
                 </div> 
             />
         </Routes>
