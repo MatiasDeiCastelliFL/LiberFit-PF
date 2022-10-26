b81fcd05cd424258fe6174d63f8ab86580df85b8
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2022-10-26 13:00:09 -0300

    Actualizacion

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,15 @@
 import server from './src/app'
-import  db from './src/db'
+import db from './src/db'
 const { PORT } = process.env
 
 
-// Syncing all the models at once.
-db.conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log('%s listening at ' + PORT) // eslint-disable-line no-console
-  })
-})
+db.sync().then(() => {
+	console.log("connect to db");
+});
+
+server.listen(PORT, () => {
+	console.log("server is running on port " + PORT);
+});
 
 
 
