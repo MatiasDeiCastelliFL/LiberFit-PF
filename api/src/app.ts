524199fff8023546cc86bfdd5a4ed8fbe4f79b14
Carlos <Carlosrgonzalez88@gmail.com>
2022-10-27 01:24:03 -0500

    rutas de id and query

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,9 @@ import cors from "cors";
 import morgan from "morgan";
 import routerEmpleado from './Routes/employe';
 import routerLocacion from './Routes/location';
+import routerIdcedes from './Routes/findById';
+import routerIdEmployee from './Routes/buscarempleado';
+import routerQuery from './Routes/findByQuery';
 const server = express()
 
 server.use(cors());
@@ -12,7 +15,11 @@ server.use(morgan("dev"));
 server.use(express.json());
 server.use(express.urlencoded({extended:true}));
 
-server.use('/', routerGeneral )
+server.use('/',routerGeneral )
 server.use('/',routerEmpleado)
 server.use('/',routerLocacion)
+server.use('/',routerQuery)
+server.use('/',routerIdcedes)
+server.use('/',routerIdEmployee)
+
 export default server 
