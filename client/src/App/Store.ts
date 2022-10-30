b47d0abc6dd9c50787b3f65547c68971f4336586
ibracardozo7 <ibracardozo15@gmail.com>
2022-10-29 23:09:05 -0300

    avanze de action users

--- client/src/App/Store.ts
+++ client/src/App/Store.ts
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from './FeatureSlices/Filters/Filter'
 import modalReducer from './FeatureSlices/Modal/Modal'
+import usersReducer from "./FeatureSlices/Users/Users"
 
 
 export const store = configureStore({
     reducer: {
         filter: filterReducer,
-        modal: modalReducer
-
+        modal: modalReducer,
+        users: usersReducer
     }
 })
 
