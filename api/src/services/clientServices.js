02aba6f8c7bee2b1205dc1c9cde8f6283c554bae
Marcelo <marcelo.abarredo@gmail.com>
2022-11-01 00:15:07 -0300

    Update clients

--- api/src/services/clientServices.js
+++ api/src/services/clientServices.js
@@ -1,4 +1,5 @@
 const { Clients,Locacions } = require('../db')
+const bcrypt= require("bcrypt")
 
 const createClient = async (
     name,
@@ -57,8 +58,23 @@ const findClientByNameAndOrEmail = async (name, email) => {
     }
 };
 
-const updateClient = async (name, phone, email, password, image) => {
-    
+const updateClient = async (id, name, phone, email, password, image) => {
+    const foundClient = await Clients.findOne({
+        where: {
+            id: id
+        }
+    })
+
+    const encryptedPassword = await bcrypt.hash(password,15)
+
+    if (foundClient) {
+        if(name) await foundClient.update({name: name})
+        if(phone) await foundClient.update({phone: phone})
+        if(email) await foundClient.update({email: email})
+        if(password) await foundClient.update({password: encryptedPassword})
+        if(image) await foundClient.update({name: name})
+    }
+    return foundClient;
 };
 
 const deleteClient = async (id, name, email) => {
