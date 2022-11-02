b60ce15ad0f78118387cbd5cf83f99a6c9767c92
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2022-11-02 17:16:52 -0300

    Trabajo ruta producto finalizado

--- api/src/Routes/product.js
+++ api/src/Routes/product.js
@@ -1,10 +1,19 @@
 const {Router}= require('express')
 const {upload }= require('../config/multer.config')
-const {getProduct,postProduct,deleteProduct} = require("../controllers/productControllers");
+const {getProduct,postProduct,deleteProduct,activarProduc,inactivarProduct,CantActivo,
+    CantInacativo,
+    FiltrarProductoDesactivado,
+    FiltrarProductoActivo} = require("../controllers/productControllers");
 
 const routerProduct= Router();
 
 routerProduct.post("/product",postProduct);
 routerProduct.get("/product",getProduct);
 routerProduct.delete('/product/:id',deleteProduct)
+routerEmpleado.put("/ProductoDesactivar",inactivarProduct);
+routerEmpleado.put("/ProductoActivar",activarProduc);
+routerEmpleado.get("/CantProductoInacativo",CantInacativo)
+routerEmpleado.get("/CantProductoActivo",CantActivo)
+routerEmpleado.get("/CantProductoActivo",FiltrarProductoDesactivado)
+routerEmpleado.get("/CantProductoActivo",FiltrarProductoActivo)
 module.exports=routerProduct ;
