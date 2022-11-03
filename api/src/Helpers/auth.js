77d9f9e7cc39fa7272d15ad1fa82237c68f5e30e
Carlos <Carlosrgonzalez88@gmail.com>
2022-11-03 13:40:26 -0500

    login

--- api/src/Helpers/auth.js
+++ api/src/Helpers/auth.js
@@ -1,11 +1,20 @@
+
+const passport = require('passport')
+const {ExtractJwt}= require('passport-jwt')
+const {Strategy}=require('passport-jwt')
 const helpers={}
-helpers.isAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      
-      return next();
-    }
-    res.redirect("/");
-  };
-  
+
+passport.use(new Strategy({
+  secretOrKey:'top_secret',
+  jwtFromRequest: ExtractJwt.fromBodyField('token')
+},async (token,done)=>{
+  try {
+    return done(null, token.user)
+  } catch (error) {
+    done(error)
+  }
+}
+))
+helpers.isAuthenticated = passport.authenticate('jwt')
 
 module.exports = helpers;
