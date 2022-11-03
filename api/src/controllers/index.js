8e2f931d1d5d95ccafb363208a065036bc13891c
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2022-11-02 23:01:08 -0300

    finish

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,8 +1,4 @@
-
-<<<<<<< HEAD
-const init=(req,res)=>{res.sendFile('C:/Users/emuga/Desktop/PF/LiberFit-PF/api/src/public/login.html')}
-=======
+g
 const init=(req,res)=>{res.send("Api liberfit")}
->>>>>>> 72d73dae2d3a7d55c4b9cd564a61ae49bb2b9fcc
 
 module.exports=init;
