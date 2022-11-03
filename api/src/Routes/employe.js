4280639df05f3f82a3a81544f25246c36b13839c
Marcelo <marcelo.abarredo@gmail.com>
2022-11-03 18:25:02 -0300

    resolve conflict

--- api/src/Routes/employe.js
+++ api/src/Routes/employe.js
@@ -15,22 +15,5 @@ routerEmpleado.get("/FiltrarEmpleadoActivo",isAuthenticated,FiltrarUsuarioActivo
 routerEmpleado.get("/FiltrarEmpleadoInactivo",isAuthenticated,FiltrarUsuarioInactivo)
 routerEmpleado.get("/CantProductoInacativo",isAuthenticated,CantInacativo)
 routerEmpleado.get("/CantProductoActivo",isAuthenticated,CantActivo)
-=========
-const {postEmpleado,getEmpleado,modificarEmpleado,deleteEmployee,inactivarEmployee,activarEmployee,FiltrarUsuarioActivo,FiltrarUsuarioInactivo,CantActivo,CantInacativo,FiltrarUsuarioInactivoConSede,
-    FiltrarUsuarioActivoConSede} = require("../controllers/employeControllers");
-const routerEmpleado = Router();
 
-routerEmpleado.post("/empleado", upload ,postEmpleado);
-routerEmpleado.get("/empleado",getEmpleado);
-routerEmpleado.put("/empleado",modificarEmpleado);
-routerEmpleado.delete("/empleado",deleteEmployee);
-routerEmpleado.put("/empleadoDesactivar",inactivarEmployee);
-routerEmpleado.put("/empleadoActivar",activarEmployee);
-routerEmpleado.get("/FiltrarEmpleadoActivo",FiltrarUsuarioActivo)
-routerEmpleado.get("/FiltrarEmpleadoInactivo",FiltrarUsuarioInactivo)
-routerEmpleado.get("/CantidadEmpleadoActivo",CantActivo)
-routerEmpleado.get("/CantidadEmpleadoInactivo",CantInacativo)
-routerEmpleado.get("/FiltrarUsuarioInactivoConSede",FiltrarUsuarioInactivoConSede)
-routerEmpleado.get("/FiltrarUsuarioActivoConSede",FiltrarUsuarioActivoConSede)
->>>>>>>>> Temporary merge branch 2
 module.exports = routerEmpleado;
