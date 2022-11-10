c2b2c5e20ba0846759ad0dbca1f943e2ceada7ee
Carlos <Carlosrgonzalez88@gmail.com>
2022-11-09 22:17:58 -0500

    arreglo  de login

--- api/src/Routes/client.js
+++ api/src/Routes/client.js
@@ -6,19 +6,11 @@ const {isAuthenticated}= require('../Helpers/auth')
 const { upload } = require('../config/multer.config');
 
 
-<<<<<<< HEAD
-routerClient.get("/clients",isAuthenticated ,getClientsRequest);//QUERY: name, email
-routerClient.get("/clients/payments",isAuthenticated, getClientsPayments); 
-routerClient.post("/clients",isAuthenticated,postClientsRequest); //BODY: { name, phone, email, password, image }
-routerClient.put("/clients",upload ,isAuthenticated,putClientRequest) //BODY: { id, name, phone, email, password, image }
-routerClient.delete("/clients", isAuthenticated,deleteClientRequest); //BODY: { id, name, email }
-=======
 routerClient.get("/clients" ,getClientsRequest);//QUERY: name, email
 routerClient.get("/clients/payments",getClientsPayments); 
 routerClient.post("/clients",postClientsRequest); //BODY: { name, phone, email, password, image }
 routerClient.put("/clients",upload,putClientRequest) //BODY: { id, name, phone, email, password, image }
 routerClient.delete("/clients",deleteClientRequest); //BODY: { id, name, email }
->>>>>>> 972927c6ae7dbc2584f2bc2ecec4be60a35feddf
 
 
 module.exports = routerClient;
