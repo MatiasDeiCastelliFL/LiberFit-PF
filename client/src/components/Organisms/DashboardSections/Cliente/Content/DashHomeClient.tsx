9311cc972717ce15f254db88baf58c043786f73d
Ricardo <ricardobvil116@gmail.com>
2022-11-10 20:28:22 -0600

    cambiar el archivo donde esta el CradReview a DashHome

--- client/src/components/Organisms/DashboardSections/Cliente/Content/DashHomeClient.tsx
+++ client/src/components/Organisms/DashboardSections/Cliente/Content/DashHomeClient.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import CardReview from '../../../../Molecules/CardReview/CardReview';
 
 const Payments = () => {
     return(
         <div className="w-tables overflow-hidden ">
-            
+            <CardReview/>
         </div>
     )
 }
