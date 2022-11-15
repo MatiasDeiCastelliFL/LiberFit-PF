3e7b5ac9e8a04dc2a42593313ff6e9f6c88b9dc3
CrisD3v <crisdevenginer@gmail.com>
2022-11-15 13:49:56 -0500

    Finish timer

--- client/src/components/Organisms/Timer/Timer.tsx
+++ client/src/components/Organisms/Timer/Timer.tsx
@@ -1,68 +1,111 @@
 import React, { useEffect, useRef, useState } from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import {FiPlay} from 'react-icons/fi'
-import {AiOutlinePause} from 'react-icons/ai'
+import { FiPlay } from "react-icons/fi";
+import { AiOutlinePause } from "react-icons/ai";
+import Sactive from "../../../assets/Sound/Active.mp3";
+import Sinactive from "../../../assets/Sound/Inactive.mp3";
 
-const colorLogo = '#FCA5A5'
-const redGray = '#FF0055'
+const colorLogo = "#FCA5A5";
+const redGray = "#FF0055";
 
 function Timer() {
-    const [paused , setPaused] = useState(false)
-    const [mode , setMode] = useState('pushups')
-    const [secondLeft , setSecondLeft] = useState(0)
-    const setSecondLeftRef = useRef(secondLeft)
-    const pausedRef = useRef(paused)
-    const modeRef = useRef(mode)
 
-    const exercise = ['pushups, sentadilla , diamante']
+    const exercises = [
+        { exercise: "Push Ups", reps: 10 },
+        { exercise: "lateral", reps: 10 },
+        { exercise: "diamante", reps: 10 },
+    ];
 
-    const initTimer = () => {
-        setSecondLeft( 1 * 60)
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [exercise , setExercise] = useState(exercises);
+    const [count , setCount] = useState(exercises.length);
+    const [activo, setActivo] = useState(false);
+    const [porcentaje, setPorcentaje] = useState(0);
+
+    let sound = new Audio();
+    let sound2 = new Audio();
+    sound.src = Sactive;
+    sound2.src = Sinactive;
+    
+
+    function toggle() {
+        if (activo) sound2.play();
+        else sound.play();
+        setActivo(!activo);
     }
 
-    const switchMode = () => {
-        const nextExercise = exercise.map(d => d).toString() === 'pushups' ? 'break' : 'pushups'
-        const nextSeconds = (nextExercise === 'pushups' ? 1 : 0.1) * 60
-        setMode(nextExercise)
-        setSecondLeftRef.current =  nextSeconds
-    }
+    function reset() {
 
-    const tick = () => {
-        setSecondLeftRef.current --
-        setSecondLeft( setSecondLeftRef.current)
+        if(count !== 0){
+            if(count != exercises.length){
+                setExercise(exercise.slice(1));
+            }
+            setCount(count - 1);
+        }
+
+       
+        
+        setSeconds(10);
+        setPorcentaje(0);
+        setActivo(false);
     }
+    
 
     useEffect(() => {
-        initTimer()
+        let intervalo: any;
+        let secondIntervalo: any;
+       
 
-        setInterval(() => {
-            if(pausedRef.current) return
-            if(setSecondLeftRef.current === 0) return switchMode()
+        if (activo) {
+            intervalo = setInterval(() => {
+                setSeconds((seconds) => seconds - 1);
+            }, 1000);
 
-            tick()
-        },1000)
-    })
+            secondIntervalo = setInterval(() => {
+                porcentaje < 100 &&
+                    setPorcentaje(porcentaje + 1.6666666666666667);
+            }, 1000);
+        }
+        if (seconds === 0) {
+            reset();
+            clearInterval(intervalo);
+        }
+        return () => {
+            clearInterval(intervalo);
+            clearInterval(secondIntervalo);
+        };
+    }, [activo, seconds, minutes, exercise]);
+
+    let second = seconds < 10 ? "0" + seconds : seconds
+
+    console.log(count)
+
     return (
         <div className="w-full mt-20 select-none grid grid-flow-row-dense grid-cols-12">
             <div className="col-start-5 col-span-3 mb-10">
-                <h2 className="text-5xl text-rose-700 text-center">Push up</h2>
+                <h2 className="text-5xl text-rose-700 text-center">
+                    {exercise[0].exercise}
+                </h2>
             </div>
             <div className="col-start-5 col-span-3">
-               
-               <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
-                    textColor: redGray,
-                    pathColor: redGray,
-                    trailColor: 'rgba(241,241,241,.5)',
-                })}/>
-               
+                <CircularProgressbar
+                    value={porcentaje}
+                    text={minutes + ":" + second }
+                    styles={buildStyles({
+                        textColor: redGray,
+                        pathColor: redGray,
+                        trailColor: "rgba(241,241,241,.5)",
+                    })}
+                />
             </div>
-            <div className="text-black text-5xl col-span-12 grid grid-cols-12 mt-20 ml-60">
-                <div className="col-start-4 bg-rose-700 m-auto p-2 text-white rounded-full">
-                    <span >{<FiPlay/>}</span>
-                </div>
-                <div className="col-start-6  bg-rose-700 m-auto p-2 text-white rounded-full">
-                    <span>{<AiOutlinePause/>}</span>
+            <div className="text-black text-5xl col-span-12 grid grid-cols-12 mt-20">
+                <div
+                    className="col-start-6 bg-rose-700 m-auto p-5 text-white rounded-full cursor-pointer"
+                    onClick={toggle}
+                >
+                    <span>{activo ? <AiOutlinePause /> : <FiPlay />}</span>
                 </div>
             </div>
         </div>
